f58578c030936cbd04dbe9aac949a36627e5b9df
neducod <nedunony16@gmail.com>
2026-06-25 12:33:53 +0100

    deleted demo piece of code

--- src/App.jsx
+++ src/App.jsx
@@ -168,33 +168,6 @@ function App() {
         </div>
       </section> */}
       <section className='flex justify-start flex-col gap-4 p-2'>
-        <div className='flex flex-row gap-2 p-4 items-center mb-6 bg-[#faf9fb]'>
-          <button className='text-[#766d8f]    flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group'>GET HIRED</button>
-        <button
-          class="relative cursor-pointer py-4 px-8 text-center font-barlow inline-flex justify-center text-base uppercase text-white rounded-lg border-solid transition-transform duration-300 ease-in-out group outline-offset-4 focus:outline focus:outline-2 focus:outline-white focus:outline-offset-4 overflow-hidden"
-        >
-          <span class="relative z-20 text-black">HIRE TALENT</span>
-
-          <span
-            class="absolute left-[-75%] top-0 h-full w-[50%] bg-white/20 rotate-12 z-10 blur-lg group-hover:left-[125%] transition-all duration-1000 ease-in-out"
-          ></span>
-
-          <span
-            class="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#151516] absolute h-[20%] rounded-tl-lg border-l-2 border-t-2 top-0 left-0"
-          ></span>
-          <span
-            class="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#151516] absolute group-hover:h-[90%] h-[60%] rounded-tr-lg border-r-2 border-t-2 top-0 right-0"
-          ></span>
-          <span
-            class="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#151516] absolute h-[60%] group-hover:h-[90%] rounded-bl-lg border-l-2 border-b-2 left-0 bottom-0"
-          ></span>
-          <span
-            class="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#151516] absolute h-[20%] rounded-br-lg border-r-2 border-b-2 right-0 bottom-0"
-          ></span>
-        </button>
-
-
-        </div>
 
         <div className='max-w-xl w-full flex flex-col gap-6 md:gap-8 p-4 md:p-6 text-[#0d0d2c]'>
           <div className="inline-flex gap-2 bg-[#f8f7fa] p-1.5 rounded-full self-start">
