3a1c727575ec9718a05db10a4635c7cb0f193957
neducod <nedunony16@gmail.com>
2026-06-28 00:17:14 +0100

    dynamic button of the hero section that switches done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,33 @@ function Hero(){
         switch (activeTab){
             case 'tab1':
             return(
-                <div className='p-4 bg-pink-400'>
-                    <h3>testing</h3>
+                <div className='max-w-xl w-full flex flex-col gap-6 md:gap-8 p-4 md:p-6 text-[#0d0d2c]'>
+                    <h1 className='text-left items-center text-3xl font-semibold max-w-xs'>Work with amazing talents in <span>Drawing</span></h1>
+
+                    <div className='flex flex-col gap-6 -mt-2'>
+                        <div className='flex justfiy-center items-center gap-4'>
+                            <div className='bg-pink-300 rounded-full p-1'>
+                                <IoMdCheckmark />
+                            </div>
+                            <p className='text-sm font-normal gap-4'>Generate your brief and receive proposals – for free</p>
+                        </div>
+                        <div className='flex justfiy-center items-center gap-4'>
+                            <div className='bg-pink-300 rounded-full p-1'>
+                                <IoMdCheckmark />
+                            </div>
+                            <p className='text-sm font-normal '>Browse real-world design and development work</p>
+                        </div>
+                        <div className='flex justify-center items-center gap-4'>
+                            <div className='bg-pink-300 rounded-full p-1'>
+                                <IoMdCheckmark />
+                            </div>
+                            <p className='text-sm font-normal'>Hire top-rated freelancers and agencies with confidence</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button className='font-bold h-[40] text-center p-3 rounded-4xl border-[#0d0d2c] text-white bg-[#0d0d2c]'>Start your Project Brief</button>
+                    </div>
                 </div>
             );
             case 'tab2':
@@ -28,16 +53,16 @@ function Hero(){
     return(
         <>
             <section className='flex justify-start flex-col gap-4 p-2'>
-                <div className='max-w-xl w-full flex flex-col gap-6 md:gap-8 p-4 md:p-6 text-[#0d0d2c]'>
+                <div className=''>
                     <div className='inline-flex gap-2 bg-[#f8f7fa] p-1.5 rounded-full self-start'>
-                        <button  onClick={() => setActiveTab('tab1')} className={`text-xs font-bold px-4 py-2 rounded-full border shadow-sm uppercase tracking-wider
+                        <button  onClick={() => setActiveTab('tab1')} className={`text-xs font-bold px-4 py-2 rounded-full shadow-sm uppercase tracking-wider
                         ${activeTab === 'tab1'
-                        ? 'bg-white text-[#0d0d2c] shadow-sm'
+                        ? 'bg-white text-[#0d0d2c] shadow-sm border'
                         : 'text-gray-400'}`}>Hire Talent</button>
 
-                        <button  onClick={() => setActiveTab('tab2')} className={`text-xs font-bold px-4 py-2 rounded-full border shadow-sm uppercase tracking-wider
+                        <button  onClick={() => setActiveTab('tab2')} className={`text-xs font-bold px-4 py-2 rounded-full shadow-sm uppercase tracking-wider
                         ${activeTab === 'tab2'
-                        ? 'bg-white text-[#0d0d2c] shadow-sm'
+                        ? 'bg-white text-[#0d0d2c] shadow-sm border'
                         : 'text-gray-400'}`}>get hired</button>
                     </div>
 
@@ -46,7 +71,7 @@ function Hero(){
                     </div>
                 </div>
             </section>
-            <section className='flex justify-start flex-col gap-4 p-2'>
+            {/* <section className='flex justify-start flex-col gap-4 p-2'>
                 <div className='max-w-xl w-full flex flex-col gap-6 md:gap-8 p-4 md:p-6 text-[#0d0d2c]'>
                     <div className="inline-flex gap-2 bg-[#f8f7fa] p-1.5 rounded-full self-start">
                         <button className="text-xs font-bold px-4 py-2 bg-white rounded-full border border-gray-200/80 shadow-sm uppercase tracking-wider text-[#0d0d2c]">
@@ -58,7 +83,6 @@ function Hero(){
                     </div>
 
                     <h1 className='text-left items-center text-3xl font-semibold max-w-xs'>Work with amazing talents in <span>Drawing</span></h1>
-                    {/* drawing, painting, designing */}
                     <div className='flex flex-col gap-6 -mt-2'>
                         <div className='flex justfiy-center items-center gap-4'>
                             <div className='bg-pink-300 rounded-full p-1'>
@@ -83,7 +107,7 @@ function Hero(){
                         <button className='font-bold h-[40] text-center p-3 rounded-4xl border-[#0d0d2c] text-white bg-[#0d0d2c]'>Start your Project Brief</button>
                     </div>
                 </div>
-            </section>
+            </section> */}
         </>
     )
 }
